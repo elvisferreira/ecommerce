74cd55cd5dea00fab35a16d9317f7bc77e0561b2
elvisferreira <29875340+elvisferreira@users.noreply.github.com>
2017-07-20 15:39:08 -0300

    Adicionado: CRUD (com erro Slim Framework)

--- index.php
+++ index.php
@@ -82,13 +82,34 @@ $app->get("/admin/users/create", function() {
 
 });
 
+$app->get("/admin/users/:iduser/delete", function($iduser) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser); 
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
 $app->get("/admin/users/:iduser", function($iduser) {
 
 	User::verifyLogin();
 
+	$user = new User();
+
+	$user->get((int)$iduser);
+
 	$page = new Hcode\PageAdmin();
 
-	$page->setTpl("users-update");
+	$page->setTpl("users-update", array(
+ 		"user"=>$user->getValues()
+ 	));
 
 });
 
@@ -96,17 +117,35 @@ $app->post("/admin/users/create", function() {
 
 	User::verifyLogin();
 
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /admin/users");
+	exit;
+
 });
 
 $app->post("/admin/users/:iduser", function($iduser) {
 
 	User::verifyLogin();
 
-});
+	$user = new User();
 
-$app->delete("/admin/users/:iduser", function($iduser) {
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
 
-	User::verifyLogin();
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
 
 });
 
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -10,7 +10,7 @@ class User extends Model {
 	const SESSION = "User";
 
 	protected $fields = [
-		"iduser", "idperson", "deslogin", "despassword", "inadmin", "dtergister"
+		"iduser", "idperson", "deslogin", "despassword", "inadmin", "dtregister"
 	];
 
 	public static function login($login, $password):User
@@ -78,6 +78,64 @@ class User extends Model {
 
 		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING (idperson) ORDER BY b.desperson");
 
+	}
+
+	public function save() {
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)",array(
+
+				":desperson"=>$this->getdesperson(),
+				":deslogin"=>$this->getdeslogin(),
+				":despassword"=>$this->getdespassword(),
+				":desemail"=>$this->getdesemail(),
+				":nrphone"=>$this->getnrphone(),
+				":inadmin"=>$this->getinadmin()
+
+			));
+
+		$this->setData($results[0]);
+
+	}
+
+	public function get($iduser)
+	{
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) where a.iduser = :iduser", array(
+				":iduser"=>$iduser
+			));
+  
+		$this->setData($results[0]);
+  	}
+
+  	public function update()
+ 	{
+ 		$sql = new SQL();
+ 
+ 		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone,:inadmin)",array(
+ 			 	":iduser"=>$this->getiduser(),
+ 				":desperson"=>$this->getdesperson(),
+ 				":deslogin"=>$this->getdeslogin(),
+ 				":despassword"=>$this->getdespassword(),
+ 				":desemail"=>$this->getdesemail(),
+ 				":nrphone"=>$this->getnrphone(),
+ 				":inadmin"=>$this->getinadmin()
+ 
+ 			));
+ 
+			$this->setData($results[0]);
+ 
+	}
+
+	public function delete()
+	{
+		$sql = new SQL();
+		
+		$sql->query("CALL sp_users_delete(:iduser)",array(
+				":iduser"=>$this->getiduser()
+			));
 	}
 
 }
