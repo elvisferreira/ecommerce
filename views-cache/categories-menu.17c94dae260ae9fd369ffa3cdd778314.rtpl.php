34542dddc843df00131d5d6a695353f4fb3f05d3
Elvis Ferreira <elvisferreira@hotmail.com>
2017-09-12 13:29:42 -0300

    Site categorias (Erro CSS)

---
+++ views-cache/categories-menu.17c94dae260ae9fd369ffa3cdd778314.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/11">Google</a></li><li><a href="/categories/12">Apple</a></li><li><a href="/categories/13">Motorola</a></li><li><a href="/categories/14">Android</a></li><li><a href="/categories/15">Samsung</a></li>
