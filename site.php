9d132a5f9ebf3a3f0ffc8dcd255b2f63f8d7d81f
Elvis Ferreira <elvisferreira@hotmail.com>
2017-12-18 15:44:01 -0300

    Carrinho calcular FRETE

--- site.php
+++ site.php
@@ -78,7 +78,8 @@ $app->get("/cart", function(){
 	$page->setTpl("cart", [
 
 		'cart'=>$cart->getValues(),
-		'products'=>$cart->getProducts()
+		'products'=>$cart->getProducts(),
+		'error'=>Cart::getMsgError()
 
 	]);
 
@@ -133,5 +134,15 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 
 });
 
+$app->post("/cart/freight", function(){
+
+	$cart = Cart::getFromSession();
+
+	$cart->setFreight($_POST['zipcode']);
+
+	header("Location: /cart");
+	exit;
+
+});
 
 ?>
